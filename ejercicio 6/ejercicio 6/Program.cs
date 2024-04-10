70a15467157d9c6cbc1e34bea0d314c8ac4ab69d
luchorivardo <luchorivardo@gmail.com>
2024-04-10 20:18:07 -0300

    ejercicio 6

---
+++ ejercicio 6/ejercicio 6/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("ingrese un numero: ");
+float numero;
+if (!float.TryParse(Console.ReadLine(), out numero))
+{
+    Console.WriteLine("dato incorrecto");
+}
+else
+{
+    if (numero > 0 ) 
+    {
+        Console.WriteLine("es positivo");
+
+    }
+    else
+    {
+        if (numero == 0)
+        {
+            Console.WriteLine("es cero");
+        }
+        else
+        {
+            Console.WriteLine("es negativo");
+        }
+        
+    }
+
+}
