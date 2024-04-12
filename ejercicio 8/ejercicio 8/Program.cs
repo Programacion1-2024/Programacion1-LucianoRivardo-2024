a78ee1afb4a3badefdb1a327e08c3c52297cc5f5
luchorivardo <luchorivardo@gmail.com>
2024-04-12 10:44:49 -0300

    ejercicio 8

---
+++ ejercicio 8/ejercicio 8/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Console.WriteLine("ingresar primer lado");
+float lado1, lado2, lado3, perimetro;
+if (!float.TryParse(Console.ReadLine(), out lado1))
+{
+    Console.WriteLine("dato incorrecto");
+}
+else
+{
+    Console.WriteLine("ingresar segundo lado");
+    if (!float.TryParse(Console.ReadLine(), out lado2))
+    {
+        Console.WriteLine("segundo dato incorrecto");
+    }
+    else
+    {
+        Console.WriteLine("ingresar tercer lado");
+        if (!float.TryParse(Console.ReadLine(), out lado3))
+        {
+            Console.WriteLine("tercer dato incorrecto");
+        }
+        else
+        {
+            perimetro = lado1 + lado2 + lado3;
+            Console.WriteLine("el perimetro del triangulo es: " + perimetro);
+        }
+    }
+}
