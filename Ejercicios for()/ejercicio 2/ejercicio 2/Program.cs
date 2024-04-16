c9ba0c7cc5398016567adaf6cffcde90943aa720
luchorivardo <luchorivardo@gmail.com>
2024-04-16 17:22:14 -0300

    ejercicios for 2

---
+++ Ejercicios for()/ejercicio 2/ejercicio 2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Se necesita calcular el promedio de 8 números ingresados por el usuario.
+
+Console.WriteLine("ingresar 8 numeros: ");
+
+int suma = 0;
+for (int i = 1; i <= 8; i++)
+{
+    Console.WriteLine($"ingresar valor n° {i}: ");
+    if (!int.TryParse( Console.ReadLine(), out int numero))
+    {
+        Console.WriteLine("dato incorrecto");
+        i--;
+    }
+    else
+    {
+        suma += numero;
+    }
+
+}
+int resultado = (suma / 8);
+Console.WriteLine($"el promedio es {resultado}");
