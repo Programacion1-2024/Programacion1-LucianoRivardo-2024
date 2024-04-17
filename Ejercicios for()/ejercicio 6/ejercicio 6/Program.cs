f91198ec6c22ad48ec19bcf5d1952bf6ae2a2796
luchorivardo <luchorivardo@gmail.com>
2024-04-17 19:20:20 -0300

    ejercicio 6

---
+++ Ejercicios for()/ejercicio 6/ejercicio 6/Program.cs
@@ -0,0 +1,15 @@
+﻿//El usuario ingresa un número N y se debe mostrar como resultado el factorial de dicho número.
+int factorial = 1;
+Console.WriteLine("ingrese un numero: ");
+if (!int.TryParse(Console.ReadLine(), out int numero))
+{
+    Console.WriteLine("dato incorrecto, reintente");
+}
+else
+{
+    for (int i = 1; i <= numero; i++)
+    {
+            factorial *= i;
+    }
+}
+Console.WriteLine($"el factorial de {numero} es: {factorial}");
