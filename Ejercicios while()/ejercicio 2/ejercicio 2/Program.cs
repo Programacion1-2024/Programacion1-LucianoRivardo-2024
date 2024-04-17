c1647e3c632e22bf505a9e2c8901b3b6f13cb2fe
luchorivardo <luchorivardo@gmail.com>
2024-04-17 20:31:32 -0300

    ejercicio 2

---
+++ Ejercicios while()/ejercicio 2/ejercicio 2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Siguiendo el ejercicio anterior, calcular el promedio de los valores ingresados.
+int i = 0;
+float valor;
+Console.WriteLine("ingresar un valor");
+if (!float.TryParse(Console.ReadLine().Replace(".", ","), out valor))
+{
+    Console.WriteLine("valor incorrecto");
+}
+else
+{
+    float total = valor;
+    while (valor != 0)
+    {
+        
+        Console.WriteLine("ingresar un valor");
+        if (float.TryParse(Console.ReadLine().Replace(".", ","), out valor))
+        {
+            total = total + valor;
+            i++;
+        }
+        else
+        {
+            Console.WriteLine("valor incorrecto");
+        }
+    }
+    Console.WriteLine("el promedio de los datos ingresados es: " + (total/i));
+}
+
