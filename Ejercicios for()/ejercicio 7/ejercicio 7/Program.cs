d3567ebe63e6812a950a9a8f2a25256c21928679
luchorivardo <luchorivardo@gmail.com>
2024-04-17 19:34:16 -0300

    ejercicio 7

---
+++ Ejercicios for()/ejercicio 7/ejercicio 7/Program.cs
@@ -0,0 +1,15 @@
+﻿// El usuario ingresa un número entero positivo N
+// y se deben mostrar todos los números enteros positivos pares desde 0 (incluido) hasta N
+
+Console.WriteLine("ingresar un numero: ");
+if (!int.TryParse(Console.ReadLine(), out int value))
+{
+    Console.WriteLine("dato invalido intente otra vez");
+}
+else
+{
+    for (int i = 0; i <= value;i+=2) //i suma de a 2 numeros y siempre sera par
+    {
+            Console.WriteLine(i);
+    }
+}
