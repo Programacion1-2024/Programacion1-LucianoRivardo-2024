ec40b0941a4a38c60a21a1e455af7f4595e56938
luchorivardo <luchorivardo@gmail.com>
2024-04-17 20:03:09 -0300

    ejercicio 1 while

---
+++ Ejercicios while()/ejercicio 1/ejercicio 1/Program.cs
@@ -0,0 +1,34 @@
+﻿// El usuario ingresará valores positivos, cuando no tenga más valores ingresará 0
+// (cero).El sistema deberá informar la suma de los ingresados.
+
+
+Console.WriteLine("ingresar un valor");
+if (!float.TryParse(Console.ReadLine().Replace(".",","), out float valor))
+{
+    Console.WriteLine("valor incorrecto");
+}
+else
+{
+    float total = valor;
+    while (valor != 0)
+    {
+        Console.WriteLine("ingresar un valor");
+        if (float.TryParse(Console.ReadLine().Replace(".",","), out float numero))
+        {
+            if (numero == 0)
+            {
+                valor = 0;
+            }
+            else
+            {
+                total = total + numero;
+                
+            }
+        }
+        else
+        {
+            Console.WriteLine("valor incorrecto");
+        }
+    }
+    Console.WriteLine(total);
+}
