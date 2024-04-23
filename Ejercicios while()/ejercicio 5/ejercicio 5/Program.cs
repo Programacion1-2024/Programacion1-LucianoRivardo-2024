13c072c4a38e3ee3e0795d097f5416704a0f12f2
luchorivardo <luchorivardo@gmail.com>
2024-04-23 18:02:24 -0300

    ejercicio 5

---
+++ Ejercicios while()/ejercicio 5/ejercicio 5/Program.cs
@@ -0,0 +1,27 @@
+﻿// Para calcular el consumo total de un cliente, el usuario primero ingresará cuántos
+// productos compró; luego el sistema solicitará que ingrese el costo de cada uno de
+// esos productos consumidos; al terminar informará el total adeudado por el cliente.
+int cantidad;
+int deuda = 0;
+Console.WriteLine("ingrese cantidad de productos comprados: ");
+if (!int.TryParse(Console.ReadLine(), out cantidad))
+{
+    Console.WriteLine("error ingrese un dato correcto.");
+}
+else
+{
+    while (cantidad != 0)
+    {
+        Console.WriteLine($"ingresar valor de producto n° {cantidad} ");
+        if (!int.TryParse(Console.ReadLine(), out int valor))
+        {
+            Console.WriteLine("error ingrese un dato correcto.");
+        }
+        else
+        {
+            deuda += valor;
+            cantidad--;
+        }
+    }
+}
+Console.WriteLine($"el total adeudado es: " + deuda);
