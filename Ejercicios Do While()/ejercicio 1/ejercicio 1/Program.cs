485f62b0437d0575d2008831bd913189a6b1c220
luchorivardo <luchorivardo@gmail.com>
2024-04-24 19:24:32 -0300

    ejercicio 1

---
+++ Ejercicios Do While()/ejercicio 1/ejercicio 1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Para el ingreso a un sistema se necesita solicitar nombre de usuario y clave, repetir
+// este ingreso hasta que el usuario ingrese la clave correcta: 667788.
+
+using System.Diagnostics.Contracts;
+
+string usuario;
+int contra;
+do
+{
+    Console.WriteLine("ingrese nombre de usuario: ");
+    usuario = Console.ReadLine();
+    Console.WriteLine("ingrese la contraseña: ");
+    if ((!int.TryParse(Console.ReadLine(), out contra)))
+    {
+        Console.WriteLine("contraseña incorrecta, intente otra vez.");
+    }
+}
+while (contra != 667788);
+{
+    Console.WriteLine("contraseña aceptada.");
+}
