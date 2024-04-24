d7da64dffe0586adceb25c9194fe4d46836a5f7f
luchorivardo <luchorivardo@gmail.com>
2024-04-24 19:38:25 -0300

    ejercicio 2

---
+++ Ejercicios Do While()/ejercicio 2/ejercicio 2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Se le solicitará ingresar un número al usuario, se repetirá el pedido hasta que
+// ingrese un positivo.
+
+int numero;
+do
+{
+    Console.WriteLine("ingrese un numero: ");
+    if (!int.TryParse(Console.ReadLine(), out numero))
+    {
+        Console.WriteLine("dato incorrecto, intente otra vez ");
+        numero = -1;
+    }
+}
+while (numero < 0);
+{
+    Console.WriteLine("se ingreso un numero positivo :) ");
+}
