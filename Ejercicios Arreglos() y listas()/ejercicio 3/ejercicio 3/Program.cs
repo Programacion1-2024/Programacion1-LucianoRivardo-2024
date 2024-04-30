d04c4ea0302e612047a8d752ddbb3618fa4c85ff
luchorivardo <luchorivardo@gmail.com>
2024-04-30 18:14:58 -0300

    ejercicio 3

---
+++ Ejercicios Arreglos() y listas()/ejercicio 3/ejercicio 3/Program.cs
@@ -0,0 +1,4 @@
+﻿//Ejercicio 3: Ingresar datos en una lista y mostrarlos
+//Consigna: El usuario debe ingresar 5 números enteros. El programa debe almacenar estos
+//números en una lista y luego mostrarlos en la consola.
+
