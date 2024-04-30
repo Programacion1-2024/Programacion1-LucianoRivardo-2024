e767d34470a064fccd409f1b25d845ed064ed9ae
luchorivardo <luchorivardo@gmail.com>
2024-04-30 18:10:30 -0300

    ejercicios 1 y 2

---
+++ Ejercicios Arreglos()/ejercicio 1/ejercicio 1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Ejercicio 1: Ingresar datos en un arreglo y mostrarlos
+//Consigna: El usuario debe ingresar 5 números enteros. El programa debe almacenar estos
+//números en un arreglo y luego mostrarlos en la consola.
+
+int[] numeros = new int[5];
+
+for  (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine("ingrese un numero: ");
+    if (!int.TryParse(Console.ReadLine(), out int numero))
+    {
+        Console.WriteLine("error dato incorrecto"); 
+    }
+    else
+    {
+        numeros[i] = numero;
+    }
+}
+for (int i = 0;i < numeros.Length; i++)
+{
+    Console.WriteLine($"el numero posicion {i} es: " + numeros[i]);
+}
---
+++ Ejercicios Arreglos()/ejercicio 2/ejercicio 2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Ejercicio 2: Calcular promedio de notas utilizando un arreglo
+//Consigna: El usuario debe ingresar 5 notas. El programa debe calcular el promedio de estas
+//notas y mostrarlo en la consola. Utiliza un arreglo para almacenar las notas.
+
+int[] notas = new int[5];
+float promedio = 0;
+for (int i = 0; i < notas.Length; i++)
+{
+    Console.WriteLine($"ingrese la nota");
+    if (!int.TryParse(Console.ReadLine(), out int nota))
+    {
+        Console.WriteLine("error dato incorrecto");
+    }
+    else
+    {
+        notas[i] = nota;
+    }
+}
+for (int i = 0;i < notas.Length; i++)
+{
+    promedio += notas[i];
+}
+Console.WriteLine("el promedio de las notas es: " + (promedio/5));
