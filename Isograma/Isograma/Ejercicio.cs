4f2da15e243b66b78b14ba080a5f490038cf31ad
luchorivardo <luchorivardo@gmail.com>
2024-05-10 11:49:42 -0300

    .

---
+++ Isograma/Isograma/Ejercicio.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isograma
+{
+    /*
+        Consigna: 
+            Determina si una palabra o frase es un isograma.
+
+            Un isograma (también conocido como "palabra sin patrón") es una palabra o frase sin una letra repetida; sin embargo, se permite que aparezcan espacios y guiones varias veces.
+
+            Ejemplos de isogramas:
+
+            lumberjacks
+            background
+            downstream
+            six-year-old
+
+            La palabra isogramas, sin embargo, no es un isograma porque la s se repite.
+
+            TIPS: Funciones ToCharArray, IndexOf y LastIndexOf de la clase String
+    */
+
+    public static class Isogram
+    {
+        public static bool IsIsogram(string word)
+        {
+            char[] caracteres = word.ToLower().ToCharArray();
+
+            for (int i = 0; i < caracteres.Length; i++)
+            {
+                char caracterActual = caracteres[i];
+
+                if (caracterActual != ' ' && caracterActual != '-')
+                {
+                    for (int j = i + 1; j < caracteres.Length; j++)
+                    {
+                        char caracterSiguiente = caracteres[j];
+
+                        if (caracterActual == caracterSiguiente)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Pangrama/Pangrama/Ejercicio.cs
@@ -0,0 +1,38 @@
+﻿namespace Pangrama
+{
+    /*
+        Consigna: 
+            Trabajas para una empresa que vende fuentes a través de su sitio web. Les gustaría mostrar una oración diferente cada vez que alguien vea una fuente en su sitio web. Para dar una idea completa de la fuente, las oraciones aleatorias deben usar todas las letras del alfabeto inglés.
+
+            Están organizando un concurso para recibir sugerencias de frases que puedan utilizar. Usted está a cargo de verificar los envíos para ver si son válidos.
+
+            Tu tarea es descubrir si una oración es un pangrama.
+
+            Un pangrama es una oración que utiliza cada letra del alfabeto al menos una vez. No distingue entre mayúsculas y minúsculas, por lo que no importa si una letra está en minúscula (por ejemplo k) o en mayúscula (por ejemplo K).
+
+            Para este ejercicio, una oración es un pangrama si contiene cada una de las 26 letras del alfabeto inglés.
+
+            TIPS: Funciones ToCharArray y Contains de la clase String
+   */
+
+    public static class Pangram
+    {
+        public static bool IsPangram(string input)
+        {
+            input = input.ToLower();
+            char[] abecedario = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            foreach (char i in abecedario)
+            {
+                if (i != ' ' && i != '-')
+                {
+                    if (!input.Contains(i))
+                    {
+                        return false;
+                    }
+                }
+               
+            }
+            return true;
+        }
+    }
+}
